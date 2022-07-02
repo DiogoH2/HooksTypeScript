961b6cb22bfa8be26c74db60045149727d4bcd64
diogo <diogo12h91@gmail.com>
2022-07-02 10:31:37 -0300

    hooks typeScrip

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import React, { useState , useEffect, useMemo, useCallback, useRef, useContext, useImperativeHandle, useReducer } from 'react';
+import './App.css';
+
+interface User {
+  name: string;
+  login: string;
+  avatar_url: string;
+}
+
+function App() {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const [users, setUser] = useState<[User]>();
+
+  const names = useMemo(() => users?.map(user => user.name).join(', ') || '', [users]);
+
+const greeting = useCallback(
+  (user: User) => alert(`Hello ${user.name}`),
+  []
+)
+
+function focusOnInput(){
+  inputRef.current?.focus();
+}
+  
+return(
+  <form action="">
+    <input type="text" ref={inputRef}/>
+  </form>
+)
+}
+
+export default App;

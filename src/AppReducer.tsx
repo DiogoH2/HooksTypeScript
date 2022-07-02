55756f75607a8c72112acb914ad2a0e6e056bc25
diogo <diogo12h91@gmail.com>
2022-07-02 10:50:01 -0300

    useReducer

--- src/AppReducer.tsx
+++ src/AppReducer.tsx
@@ -1,7 +1,36 @@
 import React from 'react'
+import { useReducer } from 'react'
+
+
+interface Cart{
+    products: string[],
+    shipping_value?: number;
+}
+
+type CartActionType = {
+    type: 'ADD_PRODUCT' | 'REMOVE_PRODUCT'
+}
 
 export default function AppReducer() {
+    const cart = useReducer(
+        (state: Cart, action: CartActionType) =>{
+            switch(action.type){
+                case 'ADD_PRODUCT':
+                    return{
+                        ...state,
+                        products: [...state.products, 'Produto novo']
+                    }                  
+                default:
+                    return state;
+            }
+        },{
+            products: ['asd'],
+            shipping_value: 0,
+        },
+    )
   return (
-    <div>AppReducer</div>
+    <ul>
+
+    </ul>
   )
 }
